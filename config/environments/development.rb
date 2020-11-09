2fee85289cec7b1fcf07e5897c0a7bd74a45ff0d
Fanny Rojon <fanny.rojon22@gmail.com>
2020-11-09 09:54:02 +0100

    default host production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,12 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -28,8 +31,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # default url
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
