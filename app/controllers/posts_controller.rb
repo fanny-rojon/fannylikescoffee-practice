ed81c3d67aeeabc389889fa2c9533431bd2f06a4
Fanny Rojon <fanny.rojon22@gmail.com>
2020-11-14 13:10:39 +0100

    posts controller create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,19 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.reverse
   end
+
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Successfully created post!"
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "Error creating new post!"
+      render :new
+    end
   end
 end
